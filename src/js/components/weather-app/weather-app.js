d9d964ec4366a2198348d090be0818daa3a3f61d
Sarhan.Azizov <sarhan.azizov@gmail.com>
2016-08-28 22:58:36 +0300

    feature-4: add weather icons

--- src/js/components/weather-app/weather-app.js
+++ src/js/components/weather-app/weather-app.js
@@ -2,6 +2,7 @@
 
 import 'jquery';
 import Backbone from 'backbone';
+import Skycons from 'skycons';
 
 import './styles/weather-app.scss';
 import template from './templates/weather-app.hbs';
@@ -12,14 +13,25 @@ import WeatherList from '../weather-list/weather-list';
 export default Backbone.View.extend({
     el: '.weather-app',
     weatherList: new WeatherList(),
+    SkyCons: Skycons(window),
+    skyCons: new (Skycons(window))(),
 
     initialize() {
         this.render();
         this.initAutoComplete();
+
+        // TODO: temporary for see the results
+        this.addIcon();
     },
 
     initAutoComplete() {
         return new PlaceAutoComplete({ el: this.$el });
+    },
+
+    // TODO: temporary for see the results
+    addIcon() {
+        this.$el.append(this.skyCons.add("icon1", this.SkyCons.PARTLY_CLOUDY_DAY));
+        this.skyCons.play();
     },
 
     render() {
